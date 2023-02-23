d34a216546567cbc1a20b88554c43b5be64e3116
Fadil Bahtiar Lubiansyah <123623358+ftiarl@users.noreply.github.com>
2023-02-23 08:10:32 +0700

    Update soal1.js

--- soal1.js
+++ soal1.js
@@ -1,7 +1,7 @@
 const rumus = require('./rumus.js');
 
 console.log("Hasil luas persegi panjang = " + luasPP(15,10))
-console.log("Hasil keliling persegi panjang = " + KelilingPP(15,10))
+console.log("Hasil keliling persegi panjang = " + kelilingPP(15,10))
 
 console.log("Hasil luas persegi = " + luasPersegi(10))
-console.log("Hasil keliling persegi = " + KelilingPersegi(10))
+console.log("Hasil keliling persegi = " + kelilingPersegi(10))
